80bd6c1f517da9681f3f76ab1672d488b9073ca7
Ramon Amini <ograaamonnn@gmail.com>
2021-10-07 17:35:41 -0700

    Added a fragment to fulfill check in 2 requirement

---
+++ app/src/main/java/com/example/projexample/location.kt
@@ -0,0 +1,24 @@
+package com.example.projexample
+
+import android.Manifest
+import android.content.pm.PackageManager
+import android.location.Location
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.core.app.ActivityCompat
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+
+class location : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_location, container, false)
+    }
+}
