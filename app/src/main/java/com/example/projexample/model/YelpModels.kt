3bfe5c48df1bbd3177cbf0c464a01a27ff5d64bb
Ramon Amini <ograaamonnn@gmail.com>
2021-10-17 23:31:10 -0700

    Added Network layer. Succesfully retrieves restaurants from yelp api and displays them on the map.

---
+++ app/src/main/java/com/example/projexample/model/YelpModels.kt
@@ -0,0 +1,36 @@
+package com.example.projexample.model
+
+import com.google.gson.annotations.SerializedName
+
+//Might need more data, lets see if maps autofills the business info
+data class YelpSearchResult(
+    @SerializedName("total") val total: Int,
+    @SerializedName("businesses") val restaurants: List<YelpRestaurant>
+)
+
+data class YelpRestaurant(
+    @SerializedName("name") val name: String,
+    @SerializedName("image_url") val image: String,
+    @SerializedName("is_closed") val is_closed: String,
+    @SerializedName("url") val url: String,
+    @SerializedName("review_count") val reviewCount: Int,
+    @SerializedName("rating") val rating: Double,
+    @SerializedName("coordinates") val coordinates: Coordinates,
+    @SerializedName("price") val price: String,
+    @SerializedName("phone") val phone: String,
+    @SerializedName("categories") val categories: List<YelpCategory>,
+    @SerializedName("location") val location: YelpLocation
+)
+
+data class Coordinates(
+    @SerializedName("latitude") val latitude: Double,
+    @SerializedName("longitude") val longitude: Double
+)
+
+data class YelpCategory(
+    @SerializedName("title") val title: String
+)
+
+data class YelpLocation(
+    @SerializedName("address1") val address: String
+)
